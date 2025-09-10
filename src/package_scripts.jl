61bc37c77106dfb5ebfdbb956b1af037c0f37532
Hugh Coleman <h.coleman.25@abdn.ac.uk>
2025-09-10 16:10:19 +0100

    disable autodiff

--- src/package_scripts.jl
+++ src/package_scripts.jl
@@ -391,7 +391,7 @@ Inputs carried over from DifferentialEquations.jl; see [here](https://docs.sciml
 - `save_everystep`: controls whether all timepoints are saved, defaults to `true`
  
 """
-function simulate(model,param;alg=KenCarp3(autodiff=true),reltol=1e-6,abstol=1e-8, dt = 0.1, maxiters = 1e3, save_everystep = true)
+function simulate(model,param;alg=KenCarp3(autodiff=false),reltol=1e-6,abstol=1e-8, dt = 0.1, maxiters = 1e3, save_everystep = true)
     p, d, ic, l, seed, noise = returnSingleParameter(model, param)
 
     # convert reaction network to ODESystem
