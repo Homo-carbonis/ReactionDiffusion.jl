bd2dd6cc056e0169216e17713138a3e2b0da9ac1
Hugh Coleman <h.coleman.25@abdn.ac.uk>
2025-12-05 16:40:06 +0000

    Clean up unused files

--- test/optscal.jl
+++
@@ -1,8 +0,0 @@
-includet("../src/GradientDescent.jl")
-using OrdinaryDiffEqTsit5
-
-graph = random_signed_digraph(3, 0.0)
-
-model = hill_system(graph)
-params = find_turing(model,20; num_batches=100, alg=Tsit5())
-good_params = optimise_scale(model,params[1]; η=0.1, maxiters=1e7)
