596d82ea515dbbe62af261776a4c311f999f2687
Hugh Coleman <h.coleman.25@abdn.ac.uk>
2026-02-16 11:56:42 +0000

    Scale phi_xx by D.

--- src/PseudoSpectral.jl
+++ src/PseudoSpectral.jl
@@ -25,17 +25,14 @@ function pseudospectral_problem(species, reaction_rates, diffusion_rates, bounda
 
     ## Offsets for constant, non-zero flux boundary conditions.
     # For u′(0) = a, u′(1) = b,
-    # define ϕ as a smooth function so that ϕ′(0) = a, ϕ′(1) = b, and write v = u - ϕ.
+    # define ϕ as a smooth function such that ϕ′(0) = a, ϕ′(1) = b, and write v = u - ϕ.
     # Then v′(0) = 0, v′(1) = 0, so we can solve for v using DCT-I.
     a,b = eachrow(boundary_conditions)
     X = range(0.0,1.0,n)
     ϕ = X.^2 * (b'-a')/2 + X * a'
-    # ϕ′′ = b-a, so in theory Δϕ = T{ϕ′′} ∝ [(a-b), 0, 0...].
-    # For the DCT-I we have Δϕ = T{ϕ′′} ∝ [0 ; (a*(-1)^i+b*(-1)^(2i)) for i=2:n]
-    # Δϕ = [0.0 ; stack(2*(n-1)/sqrt(2*(n-1))*(a*(-1)^i+b*(-1)^(2i)) for i in 2:n; dims=1)]
-    Δϕ = collect(b'-a')
+    Δϕ = ((b-a).*diffusion_rates)' |> collect
     fϕ,_ = build_function(ϕ, bs; expression=Val{false})
-    fΔϕ,_ = build_function(Δϕ, bs; expression=Val{false})
+    fΔϕ,_ = build_function(Δϕ, ds,bs; expression=Val{false})
 
     
     R = reaction_operator(species, reaction_rates, rs, plan!)
@@ -54,7 +51,7 @@ function pseudospectral_problem(species, reaction_rates, diffusion_rates, bounda
         b = Float64[params[k] for k in bs]
         i = Float64[params[k] for k in is]
         local ϕ = fϕ(b)
-        local Δϕ = fΔϕ(b)
+        local Δϕ = fΔϕ(d,b)
         local u0 = fu0(i) - ϕ
         plan! * u0
         u0 = vec(u0)
@@ -99,11 +96,9 @@ function reaction_operator(species, reaction_rates, rs, plan!)
         copyto!(p.u, u)
         plan! * p.u
         p.u .+= p.ϕ
-        p.u .= max.(p.u,0.0)
         f!(du, p.u, p.r)
-        p.u .+= p.Δϕ
-        plan! * du
         du .+= p.Δϕ
+        plan! * du
         nothing
     end
     ODEFunction(f̂!)
@@ -130,7 +125,7 @@ struct Parameters
     r :: Vector{Float64} # Reaction parameters.
     d :: Vector{Float64} # Diffusion parameters.
     ϕ :: Matrix{Float64} # Boundary lifting function
-    Δϕ :: Matrix{Float64} # DCT{Δϕ}
+    Δϕ :: Matrix{Float64}
     attempt :: Int64 # Track number of attempts at solution.
 end
 
--- test/exp.jl
+++ test/exp.jl
@@ -20,8 +20,8 @@ b0 = @reaction_network begin
 end
 
 initial = @initial_conditions begin
-    0.0, E
-    0.0, M
+    1.0, E
+    1.0, M
 end
 
 boundary =  (b0,@reaction_network)
@@ -29,29 +29,26 @@ boundary =  (b0,@reaction_network)
 model = Model(reaction, diffusion, boundary, initial)
 
 const L1 = 100.0
-const tmax = 5e5
+const tmax = 5e4
 
-# params = dict(L=L1, Dₘ=1e1, Dₑ=1e-1, αₘ¹ = 1e-5, αₘ² = 1e0, αₑ = 1e-4, ηₘ = 1e1, βₑ = 1e-3, T=1e-3, h=4.0)
-params = dict(L=L1, Dₘ=1e1, Dₑ=1e-1, αₘ¹ = 0.0, αₘ² = 0.0, αₑ = 0, ηₘ = 100.0, βₑ = 0, T=1e-3, h=4.0)
+##
 
-timeseries_plot(model,params; dt=1.0, tspan=1000, noise=1.0, max_attempts=0, num_verts=128, normalise=false, hide_y=false, species=[:M])
+params0 = dict(L=L1, Dₘ=1e1, Dₑ=1e-1, αₘ¹ = 1e-5, αₘ² = 1e0, αₑ = 1e-4, ηₘ = 1e0, βₑ = 1e-3, T=1e-3, h=4.0)
+timeseries_plot(model,params0; dt=0.5, tspan=tmax, noise=0.01, max_attempts=0, num_verts=64, normalise=false, hide_y=false, species=[:M])
 
-
-
-params0 = dict(L=L1, Dₘ=1e1, Dₑ=1e-1, αₘ¹ = 1e-5, αₘ² = 1e0, αₑ = 1e-4, ηₘ = 1e1, βₑ = 1e-3, T=1e-3, h=4.0)
-vars = [:Dₘ,:Dₑ,:αₘ¹,:αₘ²,:αₑ,:ηₘ,:βₑ,:T]
-
+##
 function isvalid(sol)
-    u,t=sol
+    U,t=sol
+    u=U[:,1]
     n=length(u)
     maxvalid = argmax(u) == 1
     minvalid = argmin(u) == n
     midvalid = u[n÷2] > 1e-6
     quartervalid = 1 < maximum(u)/u[n÷4] < 100
     tvalid = t < tmax
+    @show [maxvalid, minvalid, midvalid, quartervalid, tvalid]
     all([maxvalid, minvalid, midvalid, quartervalid, tvalid])
 end
-
 
 
 function σ(sol)
@@ -74,35 +71,32 @@ function sample(params)
     params2[:L] *= 2
     [params,params2]
 end
+decrange(start,stop) = logrange(10.0^start,10.0^stop,stop-start+1)
 
+params = product(Dₘ=decrange(-1,1), Dₑ=decrange(-1,1), αₘ¹=decrange(-5,0), αₘ²=decrange(-3,2), αₑ=decrange(-5,0), ηₘ=decrange(-1,1), βₑ=decrange(-4,-2), T=decrange(-5,-3), h=[2.0,4.0], L=[100.0])
 
+params = rand(params, 100)
+sol = simulate(model,params; dt=1.0, tspan=5e5)
+valid = findall(isvalid, sol.u)
+
+#vars = [:Dₘ,:Dₑ,:αₘ¹,:αₘ²,:αₑ,:ηₘ,:βₑ,:T]
 # optimise(model,cost,vars,params0; sample=sample, tspan=tmax, dt=1.0, num_verts=16)
 
+##
 
 
-# timeseries_plot(model,params_c; dt=0.1, tspan=500.0, noise=1e-12, max_attempts=0, num_verts=64, normalise=false, hide_y=false)
-
-# tmax=1e3
-# decrange(start,stop) = logrange(10.0^start,10.0^stop,stop-start+1)
-# params = product(Dₘ=decrange(-1,1), Dₑ=decrange(-1,1), αₘ¹=decrange(-5,0), αₘ²=decrange(-3,2), αₑ=decrange(-5,0), ηₘ=decrange(-1,1), βₑ=decrange(-4,-2), T=decrange(-5,-3), h=[2.0,4.0], L=[100.0])
+tmax=1e3
+params = product(Dₘ=decrange(-1,1), Dₑ=decrange(-1,1), αₘ¹=decrange(-5,0), αₘ²=decrange(-3,2), αₑ=decrange(-5,0), ηₘ=decrange(-1,1), βₑ=decrange(-4,-2), T=decrange(-5,-3), h=[2.0,4.0], L=[100.0])
 # params = parameter_set.(model,params)
 
+_params = [p for (i,p) in enumerate(params) if iszero(i%1000)]
+params_s = flatmap(sample,_params) |> collect
 
-
-
-
-
-
-# _params = [p for (i,p) in enumerate(params) if iszero(i%1000)]
-# params_s = flatmap(sample,_params) |> collect
-
-# sol = simulate(model,params_s; dt=5.0, tspan=5e5, noise=0.0, num_verts=8, max_attempts=5)
-# function _isvalid(sol)
-#     ismissing(sol) && return false
-#     u,t = sol
-#     isvalid(u[:,1],t)
-# end
-# valid = findall(_isvalid, sol.u)
-# s = σ.(partition(sol[valid],2),100.0)
+sol = simulate(model,params_s; dt=5.0, tspan=5e5, noise=0.0, num_verts=8, max_attempts=5)
+function _isvalid(sol)
+    !ismissing(sol) && isvalid(sol)
+end
+valid = findall(_isvalid, sol.u)
+s = σ.(partition(sol[valid],2),100.0)
 
 
