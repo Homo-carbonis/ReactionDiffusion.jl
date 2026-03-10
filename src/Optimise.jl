fa15f27357d9755d8f1b73fbd75a4178a90e6577
Hugh Coleman <h.coleman.25@abdn.ac.uk>
2026-03-10 17:26:56 +0000

    Optimize WIP

--- src/Optimise.jl
+++ src/Optimise.jl
@@ -6,7 +6,6 @@ using ..Simulate
 using FiniteDiff
 using StatsBase: sample
 using Pipe: @pipe
-using Optim: optimize, SAMIN, Options
 using OptimizationBase, OptimizationBBO
 
 """
@@ -23,28 +22,30 @@ end
 function optimisation_problem(model, cost, vars, params_min, params_max, params0; min_cost=0.0, in_domain=x->true, sample=nothing, kwargs...)
     _simulate = simulate(model; kwargs...)
     function _cost(sol)
-        (isempty(sol) || any(ismissing, sol.u)) && return 1.0
+        # (isempty(sol) || any(ismissing, sol.u)) && return 1.0
         isnothing(sample) ? cost(only(sol.u)...) : cost(sol.u)
     end
 
     _sample = something(sample, x->[x])
 
     __cost(p,_) = @pipe p |> zip_dict(vars,_) |> merge(params0, _) |> _sample |> filter(in_domain,_) |> _simulate |> _cost
-
+    # __cost(p,_) = error("Called")
     p_min = [params_min[v] for v in vars]
     p_max = [params_max[v] for v in vars]
     p0 = [params0[v] for v in vars]
     function callback(state, cost)
         cost <= min_cost
     end
-    OptimizationProblem(__cost, p0; lb=p_min, ub=p_max, callback=callback)
+    OptimizationProblem((x,p)->__cost(x,p), p0; lb=p_min, ub=p_max, callback=callback)
+    # OptimizationProblem((x,p)->error("CALLED"), p0; lb=p_min, ub=p_max, callback=callback)
+
 end
 
 function plot_cost(model, cost, vars, params; in_domain=x->true, sample=nothing, kwargs...)
     _simulate = simulate(model; kwargs...)
     
     function _cost(sol)
-        isempty(sol.u) || any(ismissing, sol.u) && return Inf
+        # isempty(sol.u) || any(ismissing, sol.u) && return Inf
         isnothing(sample) ? cost(only(sol.u)...) : cost(sol.u)
     end
 
